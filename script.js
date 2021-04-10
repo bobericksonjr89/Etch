0e3c1cc8a6f0b252eb8f1257afedfda9954dffa6
Robert Erickson <bobericksonjr@gmail.com>
2021-04-10 19:33:20 -0400

    impliments random rgb and 10% darkening

--- script.js
+++ script.js
@@ -27,10 +27,44 @@ function createGrid(size) {
 
     const gridBlocks = document.querySelectorAll('.grid-box');
     gridBlocks.forEach(block => block.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = "black";
+        if (!e.target.style.backgroundImage) {
+            let randomColor = randomRGB();
+            let backgroundColor = `linear-gradient(${randomColor}, ${randomColor})`;
+            e.target.style.backgroundImage = backgroundColor;
+            e.target.dataset.color = backgroundColor;
+            e.target.dataset.times = "0";
+            
+        } else {
+            if (e.target.dataset.times < 10) {
+                e.target.dataset.times++;
+            }
+            let timesPassed = e.target.dataset.times;
+
+            let backgroundColor = e.target.dataset.color;
+            console.log(backgroundColor);
+
+            let newColor;
+            if (timesPassed < 10) {
+                newColor = `linear-gradient(rgba(0, 0, 0, .${timesPassed}), rgba(0, 0, 0, .${timesPassed})), ` + backgroundColor;
+            } else {
+                newColor = "linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1)), " + backgroundColor;
+            }
+            console.log(newColor);
+            e.target.style.backgroundImage = newColor;
+        }
     }))
 
 }
+
+function randomRGB() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    return `rgba(${red},${green},${blue}, 1)`;
+
+}
+
 
 function getSize() {
     document.querySelectorAll('.grid-box').forEach(block => block.style.backgroundColor = "white");
